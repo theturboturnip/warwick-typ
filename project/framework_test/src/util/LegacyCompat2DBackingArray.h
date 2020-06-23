63175d3a979c642f2edd16a1a58d1da3d885b6a5
theturboturnip <popgoestoast@gmail.com>
2020-06-23 15:07:26 +0100

    Added extra checks and gsl::span to LegacyCompat2DBackingArray

--- project/framework_test/src/util/LegacyCompat2DBackingArray.h
+++ project/framework_test/src/util/LegacyCompat2DBackingArray.h
@@ -4,32 +4,37 @@
 
 #pragma once
 
+#include "fatal_error.h"
+#include <vector>
+#include <gsl/span>
+
 template<typename T>
 class LegacyCompat2DBackingArray {
     std::vector<T> m_backing;
-    std::vector<T*> m_as2D;
+    std::vector<gsl::span<T>> m_as2D;
 
 public:
-    LegacyCompat2DBackingArray(int width, int height, T default_back) : m_backing(width*height, default_back), m_as2D(width, nullptr) {
-        for (int x = 0; x < width; x++) {
-            m_as2D[x] = &m_backing[x * height];
-        }
+    LegacyCompat2DBackingArray(int width, int height, T default_back) : m_backing(width*height, default_back), m_as2D(width) {
+        build2D(width, height);
     }
 
-    LegacyCompat2DBackingArray(const std::vector<T>& backing, int width, int height) : m_backing(backing), m_as2D(width, nullptr) {
-        for (int x = 0; x < width; x++) {
-            m_as2D[x] = &m_backing[x * height];
-        }
+    LegacyCompat2DBackingArray(const std::vector<T>& backing, int width, int height) : m_backing(backing), m_as2D(width) {
+        build2D(width, height);
     }
 
-    /*operator std::vector<T*>&() {
-        return m_as2D;
-    }*/
-    T* operator[](int index) {
+    gsl::span<T> operator[](int index) {
         return m_as2D[index];
     }
 
     const std::vector<T>& getBacking() {
         return m_backing;
+    }
+
+private:
+    void build2D(int width, int height) {
+        DASSERT(m_backing.size() == width*height);
+        for (int x = 0; x < width; x++) {
+            m_as2D[x] = gsl::span<T>(&m_backing[x * height], height);
+        }
     }
 };
--- project/framework_test/src/util/fatal_error.h
+++ project/framework_test/src/util/fatal_error.h
@@ -6,6 +6,30 @@
 
 // Fatal error function - exits the program with the given error message.
 // Useful for bailing out without requiring exceptions
+__attribute__((__format__ (__printf__, 3, 4))) // Tell the compiler to treat *fmt as a format string.
 void fatal_error(const char* filepath, int line, const char* fmt, ...);
 
-#define FATAL_ERROR(format, ...) fatal_error(__FILE__, __LINE__, format, __VA_ARGS__)
+#define FATAL_ERROR(...) fatal_error(__FILE__, __LINE__, __VA_ARGS__)
+
+#define FATAL_ERROR_IF(cond, ...) do { if (!(cond)) FATAL_ERROR(__VA_ARGS__); } while(0);
+
+#define STR(x) #x
+
+#ifndef NDEBUG
+// NDEBUG is not defined => We're in debug mode
+
+// Debug assertion which prints a custom message
+// and exits the program when the condition is false.
+#define DASSERT_M(cond, ...) FATAL_ERROR_IF(cond, __VA_ARGS__)
+// Debug assertion which prints the violated condition
+// and exits the program when the condition is false
+#define DASSERT(cond) FATAL_ERROR_IF(cond, "Assertion failed: %s\n", STR(cond))
+
+#else
+// In Release mode, disable DASSERT
+
+// Debug assertion, disabled in release mode
+#define DASSERT_M(cond, ...)
+// Debug assertion, disabled in release mode
+#define DASSERT(cond)
+#endif
